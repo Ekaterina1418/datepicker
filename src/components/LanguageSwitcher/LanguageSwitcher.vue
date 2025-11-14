464cc9b5bfe7344c0901ba3b382339e4f869c9e1
oldboydevelop <70367618+oldboydevelop@users.noreply.github.com>
2025-11-14 17:09:58 +0200

    feat: update LanguageSwitcher to use language objects for improved structure

--- src/components/LanguageSwitcher/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher/LanguageSwitcher.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="custom-select">
     <select id="locale" class="select-locale" v-model="currentLang" @change="onChange">
-      <option v-for="lang in languages" :key="lang" :value="lang">{{ lang }}</option>
+      <option v-for="lang in languages" :key="lang.code" :value="lang.code">{{ lang.label }}</option>
     </select>
     <span class="material-symbols-outlined arrow-icon">arrow_drop_down</span>
   </div>
@@ -14,8 +14,12 @@ const emit = defineEmits<{
   (e: "update:lang", lang: string): void;
 }>();
 
-const languages = ["ru", "en", "sv"];
-const currentLang = ref<string>(languages[0] || "ru");
+const languages = [
+	{code: 'ru', label: 'Руский'},
+	{code: 'en', label: 'Английский'},
+	{code: 'sv', label: 'Шведский'},
+];
+const currentLang = ref<string>(languages[0]?.code || 'ru');
 
 const onChange = () => {
 	emit('update:lang',currentLang.value)
